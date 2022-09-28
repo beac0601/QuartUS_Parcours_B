7153cf571d5a5b7a48a94dc4c5167f952b98a35e
beac0601 <beac0601@usherbrooke.ca>
2022-09-28 16:19:55 -0400

    Debut de programmation

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
 #include <Arduino.h>
+#include <LibRobus.h>
+
+float calculVitesse(float distanceActuelle, float distanceAncienne);
+float calculErreurVitesse(float vitesseActuelle, float vitesseDesiree);
+float calculErreurCumuluee(float erreurVitesse, float erreurCumulee);
+void gestionVitesseMoteur(int moteur, float vitesseDesiree, float Kp, float Ki, float distanceAncienne, float erreurCumulee);
+float calculCorrection(float erreurVitesse, float erreurCumulee, float Ki, float Kp);
+void donnerVitesse(int moteur, float vitesseDesiree, float correction);
+
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -6,4 +16,59 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+
+//distanceActuelle(reel): lecture de l'encodeur la plus recente
+//distanceAncienne(reel): lecture de l'encodeur precedente
+//vitesse(reel): vitesse de rotation en pulse par cycle
+float calculVitesse(float distanceActuelle, float distanceAncienne){
+  float vitesse = distanceActuelle - distanceAncienne;
+  return vitesse;
+}
+
+
+
+//vitesseActuelle(reel): en pulses par cycle
+//vitesseDesiree(reel): en pulses par cycle
+//erreurVitesse(reel): en pulses par cycle
+float calculErreurVitesse(float vitesseActuelle, float vitesseDesiree){
+  float erreurVitesse = vitesseDesiree-vitesseActuelle;
+  return erreurVitesse;
+}
+
+//distanceActuelle(reel): en pulses
+//distanceDesiree(reel): en pulses
+//erreurDistance(reel): en pulses
+float calculErreurCumuluee(float erreurVitesse, float erreurCumulee){
+  float erreurCumulee =erreurCumulee+erreurVitesse;
+  return erreurCumulee;
+}
+
+
+//moteur(entier):numero du moteur
+//vitesseDesiree(reel): en pulses par cycle
+//Kp(reel)
+//Ki(reel)
+void gestionVitesseMoteur(int moteur, float vitesseDesiree, float Kp, float Ki, float* distanceAncienne, float* erreurCumulee){
+  float distanceActuelle = (float)ENCODER_Read(moteur);
+  float vitesseActuelle=calculVitesse(distanceActuelle,*distanceAncienne);
+  float erreurVitesse= calculErreurVitesse(vitesseActuelle,vitesseDesiree);
+  *erreurCumulee = calculErreurCumuluee(erreurVitesse,*erreurCumulee);
+  float correction = calculCorrection(erreurVitesse,*erreurCumulee,Kp,Ki);
+  donnerVitesse(moteur,vitesseDesiree,correction);
+
+}
+
+
+float calculCorrection(float erreurVitesse, float erreurCumulee, float Ki, float Kp){
+  float correction = erreurCumulee * Ki + erreurVitesse * Kp;
+  return correction;
+}
+
+
+//Si cycle = 50ms 50% = 260 pulses/cycle 100%=520 pulses/cycle
+void donnerVitesse(int moteur, float vitesseDesiree, float correction){
+  float vitesse = (vitesseDesiree + correction)/520;
+  MOTOR_SetSpeed(moteur, vitesse);
 }
